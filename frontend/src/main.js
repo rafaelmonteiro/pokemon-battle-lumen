f35d334968e5c9f55dca72f22d305957bd357231
Vanderlei Silva <vanderleisilva@santos.sp.gov.br>
2017-08-02 14:17:22 -0300

    Add dependencies jquery, bootstrap and loader

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import autoProgress from 'vue-auto-progress'
+import toastr from 'vue-toastr'
+
+window.jQuery = require('jquery');
+require('bootstrap');
 
 Vue.config.productionTip = false
 
@@ -10,6 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  template: '<div><App/><toastr ref="toastr"/><auto-progress/></div>',
+  components: { App, toastr, autoProgress }
 })
