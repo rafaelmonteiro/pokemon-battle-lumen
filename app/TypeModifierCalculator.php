e7ec887f893468a30694b1a059a50fc11e2f60ac
Danilo Bezerra <danilobezerra@santos.sp.gov.br>
2017-08-18 10:24:56 -0300

    Add type chart multipliers

--- app/TypeModifierCalculator.php
+++ app/TypeModifierCalculator.php
@@ -4,76 +4,58 @@ class TypeModifierCalculator {
     private $playerAttack;
     private $againstPokemon;
 
+    private $multipliers = [
+        [1,1,1,1,1,1,1,1,1,1,1,1,0.5,0,1,1,0.5,0],
+        [1,0.5,0.5,1,2,2,1,1,1,1,1,2,0.5,1,0.5,1,2,1],
+        [1,2,0.5,1,0.5,1,1,1,2,1,1,1,2,1,0.5,1,1,1],
+        [1,1,2,0.5,0.5,1,1,1,0,2,1,1,1,1,0.5,1,1,1],
+        [1,0.5,2,1,0.5,1,1,0.5,2,0.5,1,0.5,2,1,0.5,1,0.5,1],
+        [1,0.5,0.5,1,2,0.5,1,1,2,2,1,1,1,1,2,1,0.5,1],
+        [2,1,1,1,1,2,1,0.5,1,0.5,0.5,0.5,2,0,1,2,2,0.5],
+        [1,1,1,1,2,1,1,0.5,0.5,1,1,1,0.5,0.5,1,1,0,2],
+        [1,2,1,2,0.5,1,1,2,1,0,1,0.5,2,1,1,1,2,1],
+        [1,1,1,0.5,2,1,2,1,1,1,1,2,0.5,1,1,1,0.5,1],
+        [1,1,1,1,1,1,2,2,1,1,0.5,1,1,1,1,0,0.5,1],
+        [1,0.5,1,1,2,1,0.5,0.5,1,0.5,2,1,1,0.5,1,2,0.5,0.5],
+        [1,2,1,1,1,2,0.5,1,0.5,2,1,2,1,1,1,1,0.5,1],
+        [0,1,1,1,1,1,1,1,1,1,2,1,1,2,1,0.5,1,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,0.5,0],
+        [1,1,1,1,1,1,0.5,1,1,1,2,1,1,2,1,0.5,1,0.5],
+        [1,0.5,0.5,0.5,1,2,1,1,1,1,1,1,2,1,1,1,0.5,2],
+        [1,0.5,1,1,1,1,2,0.5,1,1,1,1,1,1,2,2,0.5,1]
+    ];
+
     public function __construct(Attack $playerAttack, Pokemon $against)
     {
         $this->playerAttack = $playerAttack;
         $this->againstPokemon = $against;
     }
 
-    private function calculateWaterTypeModifier(Attack $playerAttack)
+    public function calculateTypeModifier(Attack $playerAttack)
     {
-        if ($playerAttack->getType() == AttackType::ELECTRIC || $playerAttack->getType() == AttackType::GRASS){
-            return new TypeModifier(DamageType::DOUBLE_DAMAGE);
+        $defense = AttackType::getValue($this->againstPokemon->getType());
+        $attack = AttackType::getValue($playerAttack->getType());
+
+        $typeModifier = new TypeModifier(DamageType::NORMAL);
+        $multiplier = $this->multipliers[$attack][$defense];
+
+        if ($multiplier > 0) {
+            if ($multiplier > 1)
+                $typeModifier = new TypeModifier(DamageType::DOUBLE_DAMAGE);
+            else if ($multiplier < 1)
+                $typeModifier = new TypeModifier(DamageType::HALF_DAMAGE);
+        } else {
+            $typeModifier = new TypeModifier(DamageType::NO_DAMAGE);
         }
 
-        if ($playerAttack->getType() == AttackType::WATER || $playerAttack->getType() == AttackType::TYPE_FIRE) {
-            return new TypeModifier(DamageType::HALF_DAMAGE);
-        }
+        return $typeModifier;
     }
 
-    private function calculateGrassTypeModifier(Attack $playerAttack)
-    {
-        if ($playerAttack->getType() == AttackType::FIRE) {
-            return new TypeModifier(DamageType::DOUBLE_DAMAGE);
-        }
-
-        if ($playerAttack->getType() == AttackType::GRASS
-            || $playerAttack->getType() == AttackType::WATER
-            || $playerAttack->getType() == AttackType::ELECTRIC) {
-            return new TypeModifier(DamageType::HALF_DAMAGE);
-        }
-    }
-
-    private function calculateFireTypeModifier(Attack $playerAttack)
-    {
-        if ($playerAttack->getType() == AttackType::WATER) {
-            return new TypeModifier(DamageType::DOUBLE_DAMAGE);
-        }
-
-        if ($playerAttack->getType() == AttackType::FIRE || $playerAttack->getType() == AttackType::GRASS) {
-            return new TypeModifier(DamageType::HALF_DAMAGE);
-        }
-    }
-
-    private function calculateElectricTypeModifier(Attack $playerAttack)
-    {
-        if ($playerAttack->getType() == AttackType::ELECTRIC) {
-            return new TypeModifier(DamageType::HALF_DAMAGE);
-        }
-    }
 
     public function calculate()
     {
         $accuracy = rand(1,100);
-        $typeModifier = new TypeModifier(DamageType::NORMAL);
-
-        switch ($this->againstPokemon->getType()) {
-            case AttackType::WATER:
-                $typeModifier = $this->calculateWaterTypeModifier($this->playerAttack);
-                break;
-
-            case AttackType::GRASS:
-                $typeModifier = $this->calculateGrassTypeModifier($this->playerAttack);
-                break;
-
-            case AttackType::FIRE:
-                $typeModifier = $this->calculateFireTypeModifier($this->playerAttack);
-                break;
-
-            case AttackType::ELECTRIC:
-                $typeModifier = $this->calculateElectricTypeModifier($this->playerAttack);
-                break;
-        }
+        $typeModifier = $this->calculateTypeModifier($this->playerAttack);
 
         if ($accuracy >= 90){
             $typeModifier->defineCritical();
