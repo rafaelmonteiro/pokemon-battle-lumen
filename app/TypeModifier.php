747608944aa199a913ae11685d7a6392f5fd2cce
Rafael Monteiro <rafaelmonteiro@santos.sp.gov.br>
2017-12-22 14:23:06 -0200

    Adding critical double damage

--- app/TypeModifier.php
+++ app/TypeModifier.php
@@ -17,19 +17,19 @@ class TypeModifier
         DamageType::DOUBLE_DAMAGE           => "It's super effective!",
         DamageType::HALF_DAMAGE             => "It's not very effective... ",
         DamageType::NO_DAMAGE               => "It's not effective ",
-        DamageType::CRITICAL_2XDAMAGE       => 'Not implemented',
-        DamageType::CRITICAL_HALF_DAMAGE    => 'Not implemented'
+        DamageType::CRITICAL_2XDAMAGE       => "It's super effective! (Critical)",
+        DamageType::CRITICAL_HALF_DAMAGE    => "It's not very effective... (Critical)"
     ];
 
     private $multipliers = [
         DamageType::NORMAL                  => 10,
         DamageType::MISSED                  => 0,
-        DamageType::CRITICAL                => 1.8,
+        DamageType::CRITICAL                => 17,
         DamageType::DOUBLE_DAMAGE           => 20,
         DamageType::HALF_DAMAGE             => 2.5,
         DamageType::NO_DAMAGE               => 0,
-        DamageType::CRITICAL_2XDAMAGE       => 0,
-        DamageType::CRITICAL_HALF_DAMAGE    => 0
+        DamageType::CRITICAL_2XDAMAGE       => 27,
+        DamageType::CRITICAL_HALF_DAMAGE    => 5
     ];
 
     public function __construct($id)
@@ -41,6 +41,19 @@ class TypeModifier
 
     public function defineCritical()
     {
+        if($this->id == DamageType::DOUBLE_DAMAGE){
+            $this->id = DamageType::CRITICAL_2XDAMAGE;
+            $this->description = $this->descriptions[DamageType::CRITICAL_2XDAMAGE];
+            $this->multiplier = $this->multipliers[DamageType::CRITICAL_2XDAMAGE];        
+            return;
+        }
+        if($this->id == DamageType::HALF_DAMAGE){
+            $this->id = DamageType::CRITICAL_HALF_DAMAGE;
+            $this->description = $this->descriptions[DamageType::CRITICAL_HALF_DAMAGE];
+            $this->multiplier = $this->multipliers[DamageType::CRITICAL_HALF_DAMAGE];        
+            return;
+        }
+
         $this->id = DamageType::CRITICAL;
         $this->description = $this->descriptions[DamageType::CRITICAL];
         $this->multiplier = $this->multipliers[DamageType::CRITICAL];
